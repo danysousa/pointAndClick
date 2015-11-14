43da922664c17175e8799a656ceee9fcb2b8ad84
Jean <jflorimo@student.42.fr>
2015-11-14 19:09:00 +0100

    update map

---
+++ Assets/Scripts/environment/auto_lights.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class auto_lights : MonoBehaviour {
+
+	public float distanceLimit = 16f;
+	private GameObject player;
+	private Light currentLight;
+	private bool checkingDistance = false;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+		currentLight = GetComponent<Light> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!checkingDistance)
+			StartCoroutine(this.enableLight());
+	}
+
+	IEnumerator enableLight()
+	{
+		checkingDistance = true;
+		yield return new WaitForSeconds(0.3f);
+		float distance = Vector3.Distance(transform.position, player.transform.position);
+		currentLight.enabled = (distance <= distanceLimit);
+		checkingDistance = false;
+	}
+}
---
+++ Assets/Scripts/environment/shaky_lights.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shaky_lights : MonoBehaviour {
+	private Light currentLight;
+	private bool checkingDistance = false;
+	// Use this for initialization
+	void Start () {
+		currentLight = GetComponent<Light> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!checkingDistance)
+			StartCoroutine(this.enableLight());
+	}
+	IEnumerator enableLight()
+	{
+		checkingDistance = true;
+		currentLight.enabled = true;
+		yield return new WaitForSeconds(Random.Range(5f, 10f));
+		for (int i = 0; i < 3; i++) {
+			currentLight.enabled = false;
+			yield return new WaitForSeconds (0.1f);
+			currentLight.enabled = true;
+			yield return new WaitForSeconds (0.1f);
+		}
+		checkingDistance = false;
+	}
+}
