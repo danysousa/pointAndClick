7b7d3a958912ed3a06efe146ac6878c25978740f
Benjamin Repingon <repingonbenjamin@gmail.com>
2015-11-15 19:22:52 +0100

    corect Standar Assets

--- Assets/Standard Assets/ParticleSystems/Scripts/Hose.cs
+++ Assets/Standard Assets/ParticleSystems/Scripts/Hose.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System;
 using UnityEngine;
 
@@ -34,40 +33,3 @@ namespace UnityStandardAssets.Effects
         }
     }
 }
-=======
-using System;
-using UnityEngine;
-
-
-namespace UnityStandardAssets.Effects
-{
-    public class Hose : MonoBehaviour
-    {
-        public float maxPower = 20;
-        public float minPower = 5;
-        public float changeSpeed = 5;
-        public ParticleSystem[] hoseWaterSystems;
-        public Renderer systemRenderer;
-
-        private float m_Power;
-
-
-        // Update is called once per frame
-        private void Update()
-        {
-            m_Power = Mathf.Lerp(m_Power, Input.GetMouseButton(0) ? maxPower : minPower, Time.deltaTime*changeSpeed);
-
-            if (Input.GetKeyDown(KeyCode.Alpha1))
-            {
-                systemRenderer.enabled = !systemRenderer.enabled;
-            }
-
-            foreach (var system in hoseWaterSystems)
-            {
-                system.startSpeed = m_Power;
-                system.enableEmission = (m_Power > minPower*1.1f);
-            }
-        }
-    }
-}
->>>>>>> 6d09be22fa957992b0c260655a72c7da0594e28a
--- Assets/Standard Assets/Utility/ForcedReset.cs
+++ Assets/Standard Assets/Utility/ForcedReset.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
@@ -16,22 +15,3 @@ public class ForcedReset : MonoBehaviour
         }
     }
 }
-=======
-using System;
-using UnityEngine;
-using UnityStandardAssets.CrossPlatformInput;
-
-[RequireComponent(typeof (GUITexture))]
-public class ForcedReset : MonoBehaviour
-{
-    private void Update()
-    {
-        // if we have forced a reset ...
-        if (CrossPlatformInputManager.GetButtonDown("ResetObject"))
-        {
-            //... reload the scene
-            Application.LoadLevelAsync(Application.loadedLevelName);
-        }
-    }
-}
->>>>>>> 6d09be22fa957992b0c260655a72c7da0594e28a
